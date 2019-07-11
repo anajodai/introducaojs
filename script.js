01c07524293469b8b769d15939fdd602d6ec357e
anajodai <anajbrancos@gmail.com>
2019-07-11 11:47:12 -0300

    introdução de js

--- script.js
+++ script.js
@@ -25,21 +25,55 @@
 //      alert('Você não é um programador :(');
 //  }
 
- //Tabela Verdade
+//  //Tabela Verdade
 
- let nomeDoAluno = prompt('Qual seu nome?');
- let materia = prompt('Digite a matéria.');
- let nota = prompt('Digite sua nota.');
+//  let nomeDoAluno = prompt('Qual seu nome?');
+//  let materia = prompt('Digite a matéria.');
+//  let nota = prompt('Digite sua nota.');
  
- let media = 6 
- if (Number(nota) >= media){
-     alert('Parabéns, ' + nomeDoAluno + ' você foi aprovado na matéria ' + materia + '!');
- }
- 
- else if (Number(nota) >= media - 0.5) {
-    alert(nomeDoAluno + ', você foi aprovado na matéria ' + materia + ' pelo sistema.')
- }
+//  let media = 6 
+//  if (Number(nota) >= media){
+//      alert('Parabéns, ' + nomeDoAluno + ' você foi aprovado na matéria ' + materia + '!');
+//  }
 
- else{
-     alert('Que pena, ' + nomeDoAluno + ', sua nota não foi suficiente na matéria ' + materia);
- }
+//  else if (Number(nota) >= media - 0.5) {
+//     alert(nomeDoAluno + ', você foi aprovado na matéria ' + materia + ' pelo sistema.')
+//  }
+
+//  else{
+//      alert('Que pena, ' + nomeDoAluno + ', sua nota não foi suficiente na matéria ' + materia);
+//  }
+
+// let vetor = ['Ana', 'Carolina', 'Lolo', false, 400, 150];
+// let ana = {
+//     nome: 'Ana Carolina',
+//     sobrenome: 'Jodai Branco Silva',
+//     idade: 17,
+//     pets: [
+//         {
+//             nome: 'Taninha',
+//             raca: 'Poodle'
+//         },
+//         {
+//             nome: 'Yellow',
+//             raca: 'Vira-Lata'
+//         }
+//     ]
+// };
+
+// let joaozinho = {
+//     nome: 'João',
+//     sobrenome: 'Zinho',
+// }
+
+let lance = (prompt('Digite quantos lances você quer?'));
+let i = 1;
+let material = '*'
+while(i <= Number(lance)){
+    console.log (material.repeat(i));
+    i++;
+}
+  
+
+
+
